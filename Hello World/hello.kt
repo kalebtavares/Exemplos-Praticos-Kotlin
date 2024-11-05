8968e231a3d4c6bcc0c9ef6dd997f02ed3823584
Kaleb Tavares <148735188+kalebtavares@users.noreply.github.com>
2024-11-05 08:17:52 -0400

    Create hello.kt
    Exemplo Prático de um dos primeiros comandos básicos da programação, nesse código foi comentado cada linha de código com explicação retirada da documentação oficial do Kotlin.

---
+++ Hello World/hello.kt
@@ -0,0 +1,9 @@
+package org.kotlinlang.play     // 1.
+fun main(){                    //  2.
+    println("Hello World!")   //   3.
+
+// 1. O código Kotlin geralmente é definido em pacotes. A especificação do pacote é opcional: se você não especificar um pacote em um arquivo de origem, seu conteúdo vai para o pacote padrão.
+// 2. Um ponto de entrada para um aplicativo Kotlin é a função principal. Você pode declará-lo sem nenhum parâmetro. O tipo de retorno não é especificado, o que significa que a função não retorna nada.
+// 3. println escreve uma linha na saída padrão. Ela é importada implicitamente. Além disso, observe que ponto e vírgula no final das linhas de código são opcionais.
+}
+
